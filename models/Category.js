0c265a8941ca8591a086b88372de335ac31fc737
Brandon <brandonperez@brandons-mbp.lan>
2022-07-05 16:34:30 -0400

    Defined columns in models.

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
