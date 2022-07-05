91b21f88cea5bca5b3606be1f3af450d888fbff5
Brandon <brandonperez@brandons-mbp.lan>
2022-07-05 16:58:30 -0400

    Added associations between models.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignkey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignkey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_id',
+  foreignkey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, {
+  through: ProductTag,
+  as: 'product_id',
+  foreignkey: 'tag_id'
+})
 
 module.exports = {
   Product,
